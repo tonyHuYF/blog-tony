d2b88902ed02289cfc5cba0bacf67e42a0baf193
huyf <724826175@qq.com>
2023-03-16 15:57:32 +0800

    添加增删改tag接口

--- admin/src/main/java/com/tony/admin/controller/TagController.java
+++ admin/src/main/java/com/tony/admin/controller/TagController.java
@@ -1,14 +1,14 @@
 package com.tony.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.tony.framework.domain.ResultBean;
+import com.tony.framework.domain.Tag;
 import com.tony.framework.domain.dto.TagListDto;
 import com.tony.framework.domain.vo.PageVo;
 import com.tony.framework.domain.vo.TagVo;
 import com.tony.framework.service.TagService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -23,6 +23,32 @@ public class TagController {
     public ResultBean<PageVo<TagVo>> getTagList(Integer pageNum, Integer pageSize, TagListDto param) {
         PageVo<TagVo> vo = tagService.getTagList(new Page(pageNum, pageSize), param);
         return new ResultBean<>(vo);
+    }
+
+    @PostMapping
+    public ResultBean<Void> insert(@RequestBody Tag tag) {
+        tagService.save(tag);
+        return new ResultBean<>();
+    }
+
+    @PutMapping
+    public ResultBean<Void> update(@RequestBody Tag tag) {
+        tagService.updateById(tag);
+        return new ResultBean<>();
+    }
+
+    @GetMapping("/{id}")
+    public ResultBean<TagVo> getById(@PathVariable Integer id) {
+        Tag tag = tagService.getById(id);
+        TagVo vo = new TagVo();
+        BeanUtil.copyProperties(tag, vo);
+        return new ResultBean<>(vo);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResultBean<Void> delete(@PathVariable Integer id) {
+        tagService.removeById(id);
+        return new ResultBean<>();
     }
 
 }
--- framework/src/main/java/com/tony/framework/domain/Tag.java
+++ framework/src/main/java/com/tony/framework/domain/Tag.java
@@ -1,5 +1,7 @@
 package com.tony.framework.domain;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
@@ -31,18 +33,22 @@ public class Tag implements Serializable {
     /**
     * 
     */
+    @TableField(fill = FieldFill.INSERT)
     private Long createBy;
     /**
     * 
     */
+    @TableField(fill = FieldFill.INSERT)
     private Date createTime;
     /**
     * 
     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Long updateBy;
     /**
     * 
     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
     /**
     * 删除标志（0代表未删除，1代表已删除）
